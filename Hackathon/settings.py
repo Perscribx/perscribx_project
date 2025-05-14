ea81d4bf2b41521de3db6ce4e31930d1481af421
Matio004 <mateuszkupis@interia.eu>
2025-05-14 02:46:51 +0200

    storage

--- Hackathon/settings.py
+++ Hackathon/settings.py
@@ -48,6 +48,7 @@ ROOT_URLCONF = 'Hackathon.urls'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -123,6 +124,12 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = '/var/perscribx/static'
+STORAGES = {
+    # ...
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
