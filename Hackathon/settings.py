22fe93cafd5dc2907f37090b6e5e1deefe5fa56b
Matio004 <mateuszkupis@interia.eu>
2025-05-15 08:47:46 +0200

    register

--- Hackathon/settings.py
+++ Hackathon/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-#4o!qptk!r=_jsei&i8i^m_li2qx&2up1!v2omy-rxws^j$r0t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['192.168.100.7', '127.0.0.1', '16.171.38.11']
 
